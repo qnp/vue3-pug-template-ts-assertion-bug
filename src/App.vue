33d327c4b8b0fda173d849c1ac79f538033af97b
François Risoud <francois.risoud@gmail.com>
2023-01-31 21:22:09 +0100

    first commit: minimal repro

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const foo = ref(1);
+</script>
+
+<!-- this is working fine-->
+<!-- <template>
+  <button @click="(foo as number)++">{{ foo }}</button>
+</template> -->
+
+<!-- this generates `Uncaught SyntaxError: Unexpected identifier 'as' at...` in HMR -->
+<template lang="pug">
+button(@click="(foo as number)++")
+  | {{ foo }}
+</template>
+
+<style>
+button {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
